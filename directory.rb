108786c4bfee48994285139de83282c6986b7cb2
Victoria <icvotria@gmail.com>
2021-11-28 23:32:08 +0000

    small changes

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-=begin 
-students =[
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-  ]
-=end
-
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit return twice."
@@ -25,7 +9,11 @@ def input_students
   while !name.empty? do 
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students."
+    if students.count == 1
+      puts "Now we have #{students.count} student."
+    else 
+      puts "Now we have #{students.count} students."
+    end
     # get another name from the user
     name = gets.chomp
   end
@@ -40,8 +28,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)"
   end
 end
 
