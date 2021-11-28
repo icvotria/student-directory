7b286c1ae77fa64fc83b02009c9079efa378af2a
Victoria <icvotria@gmail.com>
2021-11-28 17:40:17 +0000

    update to directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first, print list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, print the total numbe of students
+print "Overall, we have"
+# it's important that print() doesn't add new line characters
+print 11
+print " great students."
